552da0c10611fe825011c6475812bf4816b41cdf
Irina Anastasieva <ianastasieva@gmail.com>
2022-12-05 09:57:20 -0500

    added APIs

--- test-deploy/src/main/java/com/example/testdeploy/TestDeployApplication.java
+++ test-deploy/src/main/java/com/example/testdeploy/TestDeployApplication.java
@@ -2,12 +2,27 @@ package com.example.testdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class TestDeployApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestDeployApplication.class, args);
 	}
 
+
+	@GetMapping ("/welcome")
+	public String appWelcome() {
+		return "Welcome to my api";
+	}
+
+	@GetMapping("/names")
+	public List<String> showNames(){
+		return List.of("Sam", "Steph", "Ollie");
+	}
 }
